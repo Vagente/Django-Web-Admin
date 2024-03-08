a56b9f41f3ac3d35e90e382ff06051425818f9f1
vagente <Vagente@163.com>
2024-03-08 22:21:39 +0800

    get folder size backend, only 1 query can exist at a time

--- file_manager/consumers.py
+++ file_manager/consumers.py
@@ -1,4 +1,5 @@
 import json
+import threading
 
 from channels.generic.websocket import WebsocketConsumer
 
@@ -11,6 +12,24 @@ class FileManagerConsumer(WebsocketConsumer):
         super().__init__(*args, **kwargs)
         self.manager = None
         self.funcs = None
+        self.t = None
+        self.stop_event = threading.Event()
+
+    def send_folder_size(self, path):
+        last_item = None
+        for s in self.manager.get_dir_size():
+            if last_item is not None:
+                self.send(json.dumps([DIR_SIZE, False, s, path]))
+            last_item = s
+        self.send(json.dumps([DIR_SIZE, True, last_item, path]))
+
+    def create_thread(self, path):
+        if self.t is not None and self.t.is_alive():
+            self.stop_event.set()
+            self.t.join()
+            self.stop_event.clear()
+        self.t = threading.Thread(target=self.send_folder_size, args=(path,), daemon=True)
+        self.t.start()
 
     def receive(self, text_data=None, bytes_data=None):
         try:
@@ -19,7 +38,8 @@ class FileManagerConsumer(WebsocketConsumer):
             arr: list = json.loads(text_data)
             data_type = arr[DATA_TYPE]
             data_args = arr[DATA_ARGS]
-
+            if data_type == DIR_SIZE:
+                self.create_thread(data_args[0])
             status, res = self.funcs[data_type](*data_args)
             self.send(json.dumps([data_type, status, res]))
 
